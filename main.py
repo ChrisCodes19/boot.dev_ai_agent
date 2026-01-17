be7e65d46dd5e4e535f1acc28eb3cbbc0ec97415
ChrisCodes19 <foog15b@gmail.com>
2026-01-17 16:11:31 -0500

    command line argument functionality

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
 import os
 from dotenv import load_dotenv
 from google import genai
+import argparse
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
 if api_key is None:
     raise RuntimeError("api key not found")
 client = genai.Client(api_key=api_key)
-generated_content = client.models.generate_content(model="gemini-2.5-flash", contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.")
+parser = argparse.ArgumentParser(description="Chatbot")
+parser.add_argument("user_prompt", type=str, help="User prompt")
+args = parser.parse_args()
+
+generated_content = client.models.generate_content(model="gemini-2.5-flash", contents=args.user_prompt)
 if generated_content.usage_metadata is None:
     raise RuntimeError("failed api request")
 print(f"Prompt tokens: {generated_content.usage_metadata.prompt_token_count}")
