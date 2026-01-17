e796268af8edd6a5677529c579283636f30f8990
ChrisCodes19 <foog15b@gmail.com>
2026-01-17 15:41:39 -0500

    added usage_metadata handling

--- main.py
+++ main.py
@@ -8,6 +8,10 @@ if api_key is None:
     raise RuntimeError("api key not found")
 client = genai.Client(api_key=api_key)
 generated_content = client.models.generate_content(model="gemini-2.5-flash", contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.")
+if generated_content.usage_metadata is None:
+    raise RuntimeError("failed api request")
+print(f"Prompt tokens: {generated_content.usage_metadata.prompt_token_count}")
+print(f"Response tokens: {generated_content.usage_metadata.candidates_token_count}")
 print(generated_content.text)
 
 
