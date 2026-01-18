821cfc34832a7444458838d74635908fb5c25b40
ChrisCodes19 <foog15b@gmail.com>
2026-01-18 12:42:31 -0500

    Add message history structure with user role

--- main.py
+++ main.py
@@ -1,20 +1,29 @@
 import os
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 import argparse
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
+
 if api_key is None:
     raise RuntimeError("api key not found")
+
 client = genai.Client(api_key=api_key)
 parser = argparse.ArgumentParser(description="Chatbot")
 parser.add_argument("user_prompt", type=str, help="User prompt")
 args = parser.parse_args()
 
-generated_content = client.models.generate_content(model="gemini-2.5-flash", contents=args.user_prompt)
+messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
+
+generated_content = client.models.generate_content(
+    model="gemini-2.5-flash", contents=messages
+)
+
 if generated_content.usage_metadata is None:
     raise RuntimeError("failed api request")
+
 print(f"Prompt tokens: {generated_content.usage_metadata.prompt_token_count}")
 print(f"Response tokens: {generated_content.usage_metadata.candidates_token_count}")
 print(generated_content.text)
