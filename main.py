bcc69a10adcdd32990f780db764d83742f40558a
ChrisCodes19 <foog15b@gmail.com>
2026-01-18 13:25:05 -0500

    Add --verbose flag for detailed output

--- main.py
+++ main.py
@@ -13,7 +13,10 @@ if api_key is None:
 client = genai.Client(api_key=api_key)
 parser = argparse.ArgumentParser(description="Chatbot")
 parser.add_argument("user_prompt", type=str, help="User prompt")
+parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
 args = parser.parse_args()
+
+
 
 messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
 
@@ -24,10 +27,13 @@ generated_content = client.models.generate_content(
 if generated_content.usage_metadata is None:
     raise RuntimeError("failed api request")
 
-print(f"Prompt tokens: {generated_content.usage_metadata.prompt_token_count}")
-print(f"Response tokens: {generated_content.usage_metadata.candidates_token_count}")
-print(generated_content.text)
-
+if args.verbose == True:
+    print(f"User prompt: {args.user_prompt}")
+    print(f"Prompt tokens: {generated_content.usage_metadata.prompt_token_count}")
+    print(f"Response tokens: {generated_content.usage_metadata.candidates_token_count}")
+    print(generated_content.text)
+if args.verbose == False:
+    print(generated_content.text)
 
 
 
